6f46082696dd1b7d56c394aa49da29e037712bd8
mdsaharshital <mdsaharshitol@gmail.com>
2022-03-26 02:22:39 +0600

    cannt select multiple and random generator done

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -12,8 +12,30 @@ const Shop = () => {
         .then( data => setBottles(data))
     },[]);
     const addToCart =(selectedItem)=>{
-        const newCart =[...cart, selectedItem];
-        setCart(newCart);
+        console.log();
+        if( !cart.includes(selectedItem)){
+            const newCart =[...cart, selectedItem];
+            setCart(newCart);
+        }
+    }
+    const removeFromCart =()=>{
+        setCart([]);
+    }
+    const selectOne =()=>{
+        // const value = Math.floor(Math.random() * cart.length);
+        const value = Math.floor(Math.random()*cart.length);
+        console.log(value);
+        // if( value>1 && value<10){
+        //     const oneitem = cart.map(item=> item === value);
+        //     console.log(oneitem);
+        // setCart([value])
+        // }
+        // setCart([value])
+        // const oneCart =;
+        console.log(cart[value]);
+        console.log(cart);
+        setCart([cart[value]]);
+
     }
     return (
         <div className='main'>
@@ -29,8 +51,8 @@ const Shop = () => {
                 {
                     cart.map( item => <Cart key={item.id} cartBottle={item}></Cart>)
                 }
-            <button className='chose-one-btn'>Choose One</button>
-            <button className='chose-again-btn'>Choose Again</button>
+            <button onClick={selectOne} className='chose-one-btn'>Choose One</button>
+            <button onClick={removeFromCart} className='chose-again-btn'>Choose Again</button>
             </div>
         </div>
     );
